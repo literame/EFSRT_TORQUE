2ff3b9ba17b3b34c1bd248e26325e3971d4038f1
literame <git config --global user.email>
2024-06-13 17:33:33 -0500

    subiendo listar productos

--- EFSRT_TORQUE/Controllers/ProductosController.cs
+++ EFSRT_TORQUE/Controllers/ProductosController.cs
@@ -7,16 +7,63 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
 using EFSRT_TORQUE.Models;
+using System.Drawing;
 
 namespace EFSRT_TORQUE.Controllers
 {
     public class ProductosController : Controller
     {
+        //para listar los productos
+        IEnumerable<Productos> products()
+        {
+            List<Productos> prodTemporal = new List<Productos>();
+            SqlConnection conn = null;
+            conn = new SqlConnection(
+                ConfigurationManager.ConnectionStrings["cadena"].ConnectionString
+                );
+            conn.Open();
+
+            //definimos un comando: SELECT * FROM Productos , este listara todos los elementos de la bd
+            string query = "SELECT * FROM Productos";
+            SqlCommand cmd = new SqlCommand(query, conn);
+            
+            SqlDataReader rdr = cmd.ExecuteReader();
+
+            while (rdr.Read())
+            {
+                // Productos() => sale de el nombre definido en el enumerable IEnumerable<Productos>
+                prodTemporal.Add(new Productos()
+                {
+                    ProductoID = rdr.GetInt32(0),
+                    Nombre = rdr.GetString(1),
+                    Descripcion = rdr.GetString(2),
+                    Precio = rdr.GetDecimal(3),
+                    Stock = rdr.GetInt32(4),
+                    ProveedorID = rdr.GetInt32(5),
+                });
+
+            }
+
+
+
+            rdr.Close();
+            conn.Close();
+            return prodTemporal;
+        }
+
+
+
+
+
         // GET: Productos
         //txt saludos
         public ActionResult Index()
         {
             return View();
         }
+
+
+
+
     }
 }
