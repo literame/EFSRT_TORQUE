61c2adab916dcc02bff645aa909b055710fc6ebd
literame <git config --global user.email>
2024-06-12 23:05:23 -0500

    subida

---
+++ EFSRT_TORQUE/Scripts/controls/ocultarDiv.js
@@ -0,0 +1,13 @@
+﻿
+
+
+function toggleDiv(ventanas) {
+    var div = document.getElementById(ventanas);
+    if (div.style.display === 'none') {
+        div.style.display = '';
+    } else {
+        div.style.display = 'none';
+    }
+}
+
+//.
