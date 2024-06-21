1f38008559dabc0007ac04b36f50e167be21117a
literame <git config --global user.email>
2024-06-21 14:29:17 -0500

    se agrega procedure para el manejo de total de ventas y total de compras. se añade viewbag a la vista.

--- EFSRT_TORQUE/Controllers/HomeController.cs
+++ EFSRT_TORQUE/Controllers/HomeController.cs
@@ -7,16 +7,93 @@ using System.Data;
 using System.Data.SqlClient;
 using System.Configuration;
 using EFSRT_TORQUE.Models;
+using System.Collections;
 
 namespace EFSRT_TORQUE.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+
+        public decimal ConsultarTotalCompras()
         {
-            return View();
+            decimal total = 0;
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["MiConexion"].ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("ObtenerMontoTotalCompras", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    if (rdr.Read()) 
+                    {
+                        total = rdr.GetDecimal(0); // Leer el valor como decimal
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+            // Imprimir el total de compras en la consola
+            Console.WriteLine("Monto total de compras: " + total);
+            return total;
         }
 
+        private decimal ConsultarTotalVentas()
+        {
+            decimal total = 0;
+            using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["MiConexion"].ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("ObtenerMontoTotalVentas", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    if (rdr.Read())
+                    {
+                        total = rdr.GetDecimal(0);
+                        Console.WriteLine("Monto total de ventas leído: " + total);
+                    }
+                    else
+                    {
+                        Console.WriteLine("No se encontraron resultados.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+            return total;
+        }
+
+        public ActionResult Index()
+        {
+            decimal totalCompras = ConsultarTotalCompras();
+            decimal totalVentas = ConsultarTotalVentas();
+
+            decimal diferencia = totalVentas - totalCompras;
+
+            decimal totalsoles = diferencia * 3.80M;
+
+            ViewBag.TotalCompras = totalCompras;
+            ViewBag.TotalVentas = totalVentas;
+            ViewBag.Diferencia = diferencia;
+            ViewBag.TotalSoles = totalsoles;
+            return View();
+        }
 
         public ActionResult About()
         {
